287cc9389e7ceddd19176fcaf528e2f719ad23ea
Tess Austin <tessaustin92@gmail.com>
2021-05-31 18:04:38 -0600

    ...

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -108,7 +108,7 @@ const PhotoList = ({ category }) => {
             <div className="flex-row">
                 {currentPhotos.map((image, i) => (
                     <img
-                    src={(`../../assets/small/${category}/${i}.jpg`)}
+                    src={(`../../assets/small/${category}/${i}.jpg`).default}
                         alt={image.name}
                         className="img-thumbnail mx-1"
                         key={image.name}
